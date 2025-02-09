e324fc4787060383d9b2abebbcca5714f56b3597
chaukap <chandlerhaukap@gmail.com>
2025-02-09 11:57:10 -0700

    Automating DKIM and verification to Route53

--- resources/main.tf
+++ resources/main.tf
@@ -1,3 +1,7 @@
+data "aws_route53_zone" "domain" {
+  name = var.domain
+}
+
 # SES Domain Identity
 resource "aws_ses_domain_identity" "main" {
   domain = var.domain
@@ -6,6 +10,25 @@ resource "aws_ses_domain_identity" "main" {
 # DKIM Records for Domain Verification
 resource "aws_ses_domain_dkim" "main" {
   domain = aws_ses_domain_identity.main.domain
+}
+
+# Add verification record to Route53
+resource "aws_route53_record" "domain_verification" {
+  zone_id = data.aws_route53_zone.domain.zone_id
+  name    = "_amazonses.${var.domain}"
+  type    = "TXT"
+  ttl     = "600"
+  records = [aws_ses_domain_identity.domain_identity.verification_token]
+}
+
+# Add DKIM records to Route53
+resource "aws_route53_record" "dkim_records" {
+  count   = 3
+  zone_id = data.aws_route53_zone.domain.zone_id
+  name    = "${element(aws_ses_domain_dkim.domain_dkim.dkim_tokens, count.index)}._domainkey.${var.domain}"
+  type    = "CNAME"
+  ttl     = "600"
+  records = ["${element(aws_ses_domain_dkim.domain_dkim.dkim_tokens, count.index)}.dkim.amazonses.com"]
 }
 
 # IAM User for SMTP Authentication
